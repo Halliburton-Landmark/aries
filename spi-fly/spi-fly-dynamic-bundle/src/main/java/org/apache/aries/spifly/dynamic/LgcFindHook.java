65d30bb3b074ce4833c0ce7f9a745b4719eb2587
Eugene Bova <Eugene.Bova@halliburton.com>
2026-02-19 17:21:35 +0200

    Removed unused example, fixed comments

--- spi-fly/spi-fly-dynamic-bundle/src/main/java/org/apache/aries/spifly/dynamic/LgcFindHook.java
+++ spi-fly/spi-fly-dynamic-bundle/src/main/java/org/apache/aries/spifly/dynamic/LgcFindHook.java
@@ -9,20 +9,17 @@ import org.osgi.framework.hooks.bundle.FindHook;
 /**
  * A FindHook implementation that filters bundles based on a predefined list of symbolic names. Only bundles whose
  * symbolic names start with any of the specified prefixes will be retained. The list of prefixes can be configured via
- * the system property "lgc.spifly.additional.bundles", which should contain a comma-separated list of bundle symbolic
- * name prefixes.
+ * the system property "lgc.spifly.filtered.bundles".
  */
 public class LgcFindHook implements FindHook {
 
     private static final String LGC_SPIFLY_FILTERED_BUNDLES = "lgc.spifly.filtered.bundles";
     static String[] shouldBeActivatedBundles = null;
-    /*
-     * Initialize the list of bundle symbolic name prefixes to be activated from a system property. The system property
-     * "lgc.spifly.filtered.bundles" should contain a comma-separated list We expect "org.eclipse.jetty.http",
-     * "slf4j.api", "org.mortbay.jasper.apache-jsp", "com.lgc.dsp.logging.slf4j.eclipse"
-     */
 
-    static private void initShouldBeActivatedBundles() {
+    /*
+     * Initialize the list of bundle symbolic name prefixes to be activated from a system property.
+     */
+    private static void initShouldBeActivatedBundles() {
         String additionalBundles = System.getProperty(LGC_SPIFLY_FILTERED_BUNDLES);
         if (additionalBundles != null && !additionalBundles.trim().isEmpty()) {
             shouldBeActivatedBundles = additionalBundles.split(",");
